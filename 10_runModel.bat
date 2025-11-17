9d763c6308e9471dd781dd96468de27958d2099d
Michael Ou@SSPA <ougengxin@gmail.com>
2025-11-17 14:26:22 -0500

    add postproc for outlets

--- 10_runModel.bat
+++ 10_runModel.bat
@@ -1,5 +1,5 @@
 set lst=Output.lst
-rem bin\MODFLOW-NWT_64.exe NVIHM.nam
+bin\MODFLOW-NWT_64.exe NVIHM.nam
 
 rem export budget
 findstr /R ^
@@ -13,6 +13,8 @@ findstr /R ^
   /C:"   1  267  127   127     5" ^
   /C:"   1  139   81    82     1" ^
   /C:"   1  157   93    86     2" ^
+  /C:"   1  318  120   150    18" ^
+  /C:"   1  318  120   157    20" ^
   Output_SFR.out > dump
 
 echo LAYER,ROW,COL,SEG,RCH,INFLOW,LEAKAGE,OUTFLOW,OVERLAND,PRECIP,ET,STAGE,DEPTH,WIDTH,CONDCTNC,GRADIENT> dump.out
@@ -20,6 +22,18 @@ findstr /R ^
   /C:"   1  267  127   127     5" ^
   dump | sed -E -e "s/^\s+//g" -e "s/\s+/,/g"  >> dump.out
 bin\paste -d "," Postproc\gage_napa.csv Postproc\times.csv dump.out > Output_gage_1145800.csv
+
+echo LAYER,ROW,COL,SEG,RCH,INFLOW,LEAKAGE,OUTFLOW,OVERLAND,PRECIP,ET,STAGE,DEPTH,WIDTH,CONDCTNC,GRADIENT> dump.out
+findstr /R ^
+  /C:"   1  318  120   157    20" ^
+  dump | sed -E -e "s/^\s+//g" -e "s/\s+/,/g"  >> dump.out
+bin\paste -d "," Postproc\outlet_brownsvalley.csv Postproc\times.csv dump.out > Output_BrownsValleyCreek.csv
+
+echo LAYER,ROW,COL,SEG,RCH,INFLOW,LEAKAGE,OUTFLOW,OVERLAND,PRECIP,ET,STAGE,DEPTH,WIDTH,CONDCTNC,GRADIENT> dump.out
+findstr /R ^
+  /C:"   1  318  120   150    18" ^
+  dump | sed -E -e "s/^\s+//g" -e "s/\s+/,/g"  >> dump.out
+bin\paste -d "," Postproc\outlet_napa.csv Postproc\times.csv dump.out > Output_NapaRiver.csv
 
 rem flows
 echo LAYER,ROW,COL,SEG,RCH,INFLOW,LEAKAGE,OUTFLOW,OVERLAND,PRECIP,ET,STAGE,DEPTH,WIDTH,CONDCTNC,GRADIENT> dump.out
@@ -42,6 +56,10 @@ bin\tail -n +2 Output_gage_1145600_2.csv >> Output_gage.csv
 
 bin\ArrayMath -d 948 11 -cn -rn -a Output_gage_1145800.csv - 1.0 --groupby sp mean | sed -E -e "s/^\s+//g" -e "s/\s+$//g" -e "s/\s+/,/g" > Output_gage_1145800_monthly.csv
 bin\ArrayMath -d 948 11 -cn -rn -a Output_gage_1145600_2.csv - 1.0 --groupby sp mean | sed -E -e "s/^\s+//g" -e "s/\s+$//g" -e "s/\s+/,/g" > Output_gage_1145600_monthly.csv
+bin\ArrayMath -d 948 11 -cn -rn -a Output_BrownsValleyCreek.csv - 1.0 --groupby sp mean | sed -E -e "s/^\s+//g" -e "s/\s+$//g" -e "s/\s+/,/g" > Output_BrownsValleyCreek_monthly.csv
+bin\ArrayMath -d 948 11 -cn -rn -a Output_NapaRiver.csv - 1.0 --groupby sp mean | sed -E -e "s/^\s+//g" -e "s/\s+$//g" -e "s/\s+/,/g" > Output_NapaRiver_monthly.csv
 
-copy /Y /B Output_gage_1145800_monthly.csv Output_gage_monthly.csv
-bin\tail -n +2 Output_gage_1145600_monthly.csv >> Output_gage_monthly.csv
+copy /Y /B Output_gage_1145800_monthly.csv Output_flow_monthly.csv
+bin\tail -n +2 Output_gage_1145600_monthly.csv >> Output_flow_monthly.csv
+bin\tail -n +2 Output_BrownsValleyCreek_monthly.csv >> Output_flow_monthly.csv
+bin\tail -n +2 Output_NapaRiver_monthly.csv >> Output_flow_monthly.csv
